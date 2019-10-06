3117ffc1aa18490b0668f45ed0ea8629c1c4f88d
farooqam <farooq_am@hotmail.com>
2019-10-06 16:27:04 -0600

    Handling not found routes and default route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostsComponent } from './posts/posts.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full'
+  },
   {
     path: 'posts',
     component: PostsComponent
@@ -15,11 +21,15 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, PostsComponent, AboutComponent],
+  declarations: [AppComponent, NavbarComponent, PostsComponent, AboutComponent, PageNotFoundComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
